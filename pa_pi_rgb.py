02a6d269510576f820e48a0849f2cff7ce304385
wawzat <wawzat@gmail.com>
2019-11-27 15:01:47 -0800

    Added RGB version

---
+++ pa_pi_rgb.py
@@ -0,0 +1,120 @@
+import json
+import requests
+from time import sleep
+import datetime
+import RPi.GPIO as GPIO
+import sys
+import traceback
+ 
+import board
+import busio
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+lcd_columns = 16
+lcd_rows = 2
+i2c = busio.I2C(board.SCL, board.SDA)
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+lcd.clear()
+lcd.color = [0, 150, 0]
+lcd.message = "Ready"
+
+
+def write_message(message, backlightColor):
+   lcd.clear()
+   if backlightColor == 'Green':
+      lcd.color = [0, 150, 0]
+   elif backlightColor == 'Yellow':
+      lcd.color = [150, 150, 0]
+   elif backlightColor == "Red":
+      lcd.color = [150, 0, 0]
+   lcd.message = message
+   return
+
+
+def get_sensor_reading(sensor_id):
+    response = requests.get("https://www.purpleair.com/json?show=" + sensor_id)
+    sensor_reading = json.loads(response.text)
+    pm2_5_reading = sensor_reading['results'][0]['PM2_5Value']
+    return pm2_5_reading
+
+
+def calc_aqi(PM2_5):
+    # Function takes the instantaneous PM2.5 value and calculates
+    # "AQI". "AQI" in quotes as this is not an official methodology. AQI is
+    # 24 hour midnight-midnight average. May change to NowCast or other
+    # methodology in the future.
+
+    color = "Red"
+    # Truncate to one decimal place.
+    PM2_5 = int(float(PM2_5) * 10) / 10.0
+    if PM2_5 < 0:
+        PM2_5 = 0
+    #AQI breakpoints [0,    1,     2,    3    ]
+    #                [Ilow, Ihigh, Clow, Chigh]
+    pm25_aqi = {
+        'good': [0, 50, 0, 12],
+        'moderate': [51, 100, 12.1, 35.4],
+        'sensitive': [101, 150, 33.5, 55.4],
+        'unhealthy': [151, 200, 55.5, 150.4],
+        'very': [201, 300, 150.5, 250.4],
+        'hazardous': [301, 500, 250.5, 500.4],
+        'beyond_aqi': [301, 500, 250.5, 500.4]
+        }
+    try:
+        if (0.0 <= PM2_5 <= 12.0):
+            aqi_cat = 'good'
+        elif (12.1 <= PM2_5 <= 35.4):
+            aqi_cat = 'moderate'
+        elif (33.5 <= PM2_5 <= 55.4):
+            aqi_cat = 'sensitive'
+        elif (55.5 <= PM2_5 <= 150.4):
+            aqi_cat = 'unhealthy'
+        elif (150.5 <= PM2_5 <= 250.4):
+            aqi_cat = 'very'
+        elif (250.5 <= PM2_5 <= 500.4):
+            aqi_cat = 'hazardous'
+        elif (PM2_5 >= 500.5):
+            aqi_cat = 'beyond_aqi'
+        else:
+            print(" ")
+            print("PM2_5: " + str(PM2_5))
+        Ihigh = pm25_aqi.get(aqi_cat)[1]
+        Ilow = pm25_aqi.get(aqi_cat)[0]
+        Chigh = pm25_aqi.get(aqi_cat)[3]
+        Clow = pm25_aqi.get(aqi_cat)[2]
+        Ipm25 = int(round(
+            ((Ihigh - Ilow) / (Chigh - Clow) * (PM2_5 - Clow) + Ilow)
+            ))
+        if Ipm25 <= 50:
+            color = "Green"
+        elif 50 < Ipm25 <= 100:
+            color = "Yellow"
+        elif Ipm25 > 100:
+            color = "Red"   
+        return Ipm25, color
+    except Exception as e:
+        pass
+        print("error in calc_aqi() function: %s") % e
+    traceback.print_exc(file=sys.stdout)
+
+
+#sensor_id = "9208"
+sensor_id = "27815"
+
+try:
+    while 1:
+        reading = get_sensor_reading(sensor_id)
+        print(reading)
+        Ipm25, color = calc_aqi(reading)
+        message = "AQI: " + str(Ipm25)
+        print(message)
+        write_message(message, color)
+        delay_loop_start = datetime.datetime.now()
+        elapsed_time = datetime.datetime.now() - delay_loop_start
+        while elapsed_time.seconds <= 45:
+            elapsed_time = datetime.datetime.now() - delay_loop_start
+            sleep(.02)
+
+except KeyboardInterrupt:
+   sleep(.4)
+   GPIO.cleanup()

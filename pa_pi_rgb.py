33b3eecafb79070c3ec034f08536cae01dedf63c
wawzat <wawzat@gmail.com>
2020-10-27 21:25:52 -0700

    Add additional required return value in get_sensor_reading() except block

--- pa_pi_rgb.py
+++ pa_pi_rgb.py
@@ -121,10 +121,10 @@ def get_sensor_reading(connection_url):
             print("error status code not 200")
             raise requests.exceptions.RequestException
         return pm2_5_reading_avg, pm2_5_reading_live, conn_success
-    except (requests.exceptions.RequestException, ValueError) as e:
+    except requests.exceptions.RequestException as e:
         conn_success = False
         print("Request Exception: %s" % e)
-        return 0, conn_success
+        return 0, 0, conn_success
 
 
 def calc_aqi(PM2_5):
